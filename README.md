ddc2c72fb715931bf32dfc68bb60d8ddb60b6f1b
Liang457 <69960984+Liang457@users.noreply.github.com>
2022-01-14 21:35:37 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# hitokoto-下载
+
+[一言](https://hitokoto.cn/)爬取进 xls 表格
+
+请求的是 [一言国际地址](https://international.v1.hitokoto.cn)
+
+分类 等详细见[https://developer.hitokoto.cn/](https://developer.hitokoto.cn/)
+
+写入 /一言.xls 文件
+
+***
+
+依赖包：
+- requests
+- xlwt
---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import xlwt
+import json
+
+
+def main_get(frequency):
+    workbook = xlwt.Workbook(encoding='ascii')
+    worksheet = workbook.add_sheet("Main")
+
+    worksheet.write(0, 0, '序列')
+    worksheet.write(0, 1, '一言标识')
+    worksheet.write(0, 2, '一言正文')
+    worksheet.write(0, 3, '类型')
+    worksheet.write(0, 4, '一言的出处')
+
+    for i in range(1, frequency + 1):
+        text = get_one()
+        json_text = json_analysis(text)
+
+        worksheet.write(i, 0, str(i))  # 写入序列
+        worksheet.write(i, 1, json_text['id'])  # 写入一言标识
+        worksheet.write(i, 2, json_text['hitokoto'])  # 写入一言正文
+        worksheet.write(i, 3, json_text['type'])  # 写入类型
+        worksheet.write(i, 4, json_text['from'])  # 写入一言的出处
+
+        progress_bar(frequency, i, json_text['hitokoto'])
+
+    workbook.save("一言.xls")
+
+
+def get_one():
+    url = 'https://international.v1.hitokoto.cn'
+
+    word = requests.get(url)
+    word.encoding = 'utf-8'
+    return word.text
+
+
+def json_analysis(text):
+    return json.loads(text)
+
+
+def progress_bar(all_t, now_t, now_w):
+    bai_fen_bi = now_t / all_t
+    bai_fen_bi = "%.2f%%" % (bai_fen_bi * 100)
+    print('\n\n')
+    print(f'''爬取完成
+一言正文:{now_w}
+爬取进度 {now_t}/{all_t}[{bai_fen_bi}]
+''')
+
+
+main_get(int(input('爬取多少次？')))
